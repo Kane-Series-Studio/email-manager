79fe55ef129cf69ab42bb1019228b74b6e7852bf
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-25 16:13:34 -0400

    save code to db

--- html/validation/dataBase.js
+++ html/validation/dataBase.js
@@ -1,6 +1,22 @@
+function saveToDB(){
+const code = Math.floor(Math.random() * 999999) + 111111;
+var code_element = document.getElementById("code-value").innerHTML = code;
+
 const mongoose = require('mongoose')
 const codeSchema = mongoose.Schema({
     code: String
 })
 module.exports = mongoose.model("Codes", codeSchema);
 
+const mongodb = require('mongodb');
+mongoose.connect('mongodb://localhost:27017/KaneSeries', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+const newCode = new codeSchema({
+    code: code
+})
+newCode.save();
+}
+
+document.onload = saveToDB();
