2401904b9a94c46e29dfc5e0f79922d05aaf899c
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-26 09:37:35 -0400

    node http

--- email/replier.js
+++ email/replier.js
@@ -1,1 +1,25 @@
-const nodemailer = require('nodemai')
+const nodemailer = require('nodemailer');
+const config = require('./config/email.json');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: `${config.email}`,
+      pass: `${config.password}`
+    }
+  });
+  
+  var mailOptions = {
+    from: `${config.email}`,
+    to: 'rsrudd@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
