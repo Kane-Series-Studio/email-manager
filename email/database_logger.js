acd735cb86dcedf3068cf1eb26c6b10102e2b201
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-26 12:42:16 -0400

    prj reset

--- email/database_logger.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const emailSchema = mongoose.Schema({
-    email: String,
-    type: String,
-    timesent: { type: Date, default: Date.now},
-})
-
-module.exports = mongoose.model("email", emailSchema)
--- email/db_connection.js
+++
@@ -1,9 +0,0 @@
-const connectionDB = () => {
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/KaneSeries", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
-}
-exports.connectionDB = connectionDB;
